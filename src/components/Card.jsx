4a6d3ed46bad0391be30da35102577764a116958
Graziele Ribeiro <graziribeiro95@gmail.com>
2022-04-26 16:48:47 -0300

    refactor Card comṕonent

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,44 @@
+import styled from "styled-components";
+import { themeGet } from "@styled-system/theme-get";
+import { Text } from "./Text";
+
+const CardContent = styled.div`
+  display: flex;
+  height: 200px;
+  border: solid 1px ${themeGet("colors.darkGrey")};
+  justify-content: center;
+  align-items: center;
+  border-radius: ${themeGet("radii.sm")};
+  background-color: ${themeGet("colors.white")};
+  padding: ${themeGet("space.xsm")};
+`;
+
+const Img = styled.img`
+  margin: ${themeGet("space.sm")};
+  width: 90px;
+`;
+
 const Card = ({ pokemon }) => {
   return (
-    <>
+    <CardContent>
+      <Img
+        src={pokemon.sprites.other.dream_world.front_default}
+        alt={pokemon.name}
+      />
       <div>
-        <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-      </div>
-      <div>
-        <h3>#{pokemon.id}</h3>
-        <h2> {pokemon.name}</h2>
-      </div>
-      <div>
+      <Text variant="subtitle">#{pokemon.id}</Text>
+      <Text variant="title"> {pokemon.name}</Text>
+      <>
         {pokemon.types.map((type, index) => {
-          return <h4 key={index}>Type:{type.type.name} </h4>;
+          return (
+            <Text variant="text" key={index}>
+              Type:{type.type.name}{" "}
+            </Text>
+          );
         })}
+      </>
       </div>
-    </>
+    </CardContent>
   );
 };
 
