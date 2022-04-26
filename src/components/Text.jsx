783714deadcff655fc2bcf454b5f6f2660be05bc
Graziele Ribeiro <graziribeiro95@gmail.com>
2022-04-26 16:49:36 -0300

    add Text with variants

---
+++ src/components/Text.jsx
@@ -0,0 +1,26 @@
+import { variant } from "styled-system";
+import styled from "styled-components";
+import { themeGet } from "@styled-system/theme-get";
+
+export const Text = styled.p`
+  color: ${themeGet("colors.text")};
+  font-family: ${themeGet("fonts.mainFont")};
+  font-weight: ${themeGet("fontWeights.bold")};
+  ${variant({
+    variants: {
+      title: {
+        fontSize: "title",
+      },
+      subtitle: {
+        fontSize: "subtitle",
+      },
+      text: {
+        fontSize: "text",
+        fontWeight: "regular",
+      },
+      textButton: {
+        fontSize: "textButton",
+      },
+    },
+  })};
+`;
