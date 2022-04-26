c9132fc89fd45c00b61679a8c2641846e4c846c4
Graziele Ribeiro <graziribeiro95@gmail.com>
2022-04-26 10:43:03 -0300

    add fetchPokemons to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
+import Cards from "./components/Cards";
+import { useState, useEffect } from "react";
+import { getPokemons, getPokemonData } from "./loadData";
+
 function App() {
-  return <h1>POKÉDEX</h1>;
+  const [pokemons, setPokemons] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchPokemons = async () => {
+    try {
+      setIsLoading(true);
+      const data = await getPokemons();
+      const promises = data.results.map(async (pokemon) => {
+        return await getPokemonData(pokemon.url);
+      });
+
+      const results = await Promise.all(promises);
+      setPokemons(results);
+      setIsLoading(false);
+    } catch (error) {
+      console.log("requesting error", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPokemons();
+  }, []);
+
+  return (
+    <>
+      <h1>Pokédex</h1>
+      <Cards pokemons={pokemons} isLoading={isLoading} />
+    </>
+  );
 }
 
 export default App;
