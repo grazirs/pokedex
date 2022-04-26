c8211e5900c20b7022d3eb18d0297341e6f080f2
Graziele Ribeiro <graziribeiro95@gmail.com>
2022-04-26 16:49:11 -0300

    refactor Card component

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,21 +1,29 @@
 import Card from "./Card";
+import styled from "styled-components";
+import { themeGet } from "@styled-system/theme-get";
+
+const Pokedex = styled.div`
+  display: grid;
+  width: 100%;
+  height: 100%;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: ${themeGet("space.sm")};
+`;
 
 const Cards = ({ pokemons, isLoading }) => {
   return (
-    <>
-      <div>
-        {isLoading ? (
-          <h2>The Pokémons are being loaded</h2>
-        ) : (
-          <div>
-            {pokemons &&
-              pokemons.map((pokemon) => (
-                <Card key={pokemon.id} pokemon={pokemon} />
-              ))}
-          </div>
-        )}
-      </div>
-    </>
+    <Pokedex>
+      {isLoading ? (
+        <h2>The Pokémons are being loaded</h2>
+      ) : (
+        <>
+          {pokemons &&
+            pokemons.map((pokemon) => (
+              <Card key={pokemon.id} pokemon={pokemon} />
+            ))}
+        </>
+      )}
+    </Pokedex>
   );
 };
 export default Cards;
