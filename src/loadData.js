b8e84a2f73574934ab65db9fccc0d4b8a633d055
Graziele Ribeiro <graziribeiro95@gmail.com>
2022-04-26 10:40:18 -0300

    add loadData

---
+++ src/loadData.js
@@ -0,0 +1,19 @@
+const API_URL = `https://pokeapi.co/api/v2/pokemon`;
+
+export const getPokemons = async (limit = 25, offset = 0) => {
+  try {
+    const response = await fetch(`${API_URL}?limit=${limit}&offset=${offset}`);
+    return await response.json();
+  } catch (error) {
+    console.log("Error", error);
+  }
+};
+
+export const getPokemonData = async (url) => {
+  try {
+    const response = await fetch(url);
+    return await response.json();
+  } catch (error) {
+    console.log("error: ", error);
+  }
+};
