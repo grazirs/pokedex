fca51f9309dd1edb9591a4b3bd588708165ea855
grazirs <graziribeiro95@gmail.com>
2022-05-05 13:38:35 -0300

    add onSearchHandler to app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import Cards from "./components/Cards";
 import { useState, useEffect } from "react";
-import { getPokemons, getPokemonData } from "./loadData";
+import { getPokemons, getPokemonData, searchPokemons } from "./loadData";
 import theme from "./theme";
 import { Text } from "./components/Text.styles";
 import GlobalStyle from "./globalStyle";
 import { ThemeProvider } from "styled-components";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   const [pokemons, setPokemons] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [notFound, setNotFound] = useState(false);
 
   const fetchPokemons = async () => {
     try {
       setIsLoading(true);
+      setNotFound(false);
       const data = await getPokemons();
       const promises = data.results.map(async (pokemon) => {
         return await getPokemonData(pokemon.url);
@@ -30,12 +33,32 @@ function App() {
     fetchPokemons();
   }, []);
 
+  const onSearchHandler = async (pokemon) => {
+    if(!pokemon) {
+      return fetchPokemons();
+    }
+
+    setIsLoading(true)
+    setNotFound(false)
+    const result = await searchPokemons(pokemon)
+    if(!result) {
+      setNotFound(true)
+    } else {
+      setPokemons([result])
+    }
+    setIsLoading(false)
+
+  }
+
+
   return (
     <>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Text variant="title">Pokédex</Text>
-        <Cards pokemons={pokemons} isLoading={isLoading} />
+        <SearchBar onSearchHandler = {onSearchHandler} />
+        {notFound ? <h3>Pokémon not found, try again </h3>:
+        <Cards pokemons={pokemons} isLoading={isLoading} />}
       </ThemeProvider>
     </>
   );
