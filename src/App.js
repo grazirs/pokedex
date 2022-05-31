ea3a97d7616dedae9ad32554a47fb293cdeba1ab
Graziele Ribeiro <graziribeiro95@gmail.com>
2022-05-31 11:48:34 -0300

    add fetchPokemonsByType

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Cards from "./components/Cards";
 import { useState, useEffect } from "react";
-import { getPokemons, getPokemonData, searchPokemons } from "./loadData";
+import { getPokemons, getPokemonData, searchPokemons, loadPokemonsTypes, filterPokemonsByType } from "./loadData";
 import themes from "./theme";
 import GlobalStyle from "./globalStyle";
 import { ThemeProvider } from "styled-components";
@@ -8,6 +8,8 @@ import SearchBar from "./components/SearchBar";
 import Toggle from "./components/Toggle";
 import Logo from "./components/Logo";
 import { Text } from "./components/Text.styles";
+import SelectByType from "./components/SelectByType";
+import { Container } from "./components/Container.styles";
 
 const useTheme = () => {
   const localStorageTheme = () => {
@@ -35,7 +37,10 @@ function App() {
   const [totalPages, setTotalPages] = useState(0)
   const ITENS_PER_PAGE = 24;
   const { currentTheme, switchTheme } = useTheme();
+  const [search, setSearch] = useState("");
+  const [pokemonsTypes, setPokemonsTypes] = useState([]);
 
+  console.log(search)
   const fetchPokemons = async () => {
     try {
       setIsLoading(true);
@@ -44,7 +49,6 @@ function App() {
       const promises = data.results.map(async (pokemon) => {
         return await getPokemonData(pokemon.url);
       });
-
       const results = await Promise.all(promises);
       setPokemons(results);
       setIsLoading(false);
@@ -54,15 +58,28 @@ function App() {
     }
   };
 
+  const fetchPokemonsByType = async () => {
+    try {
+      const dataType = await loadPokemonsTypes();
+      const promiseType = dataType.results.map(async (type) => {
+        return await type.name
+      });
+      const resultsType = await Promise.all(promiseType);
+      setPokemonsTypes(resultsType);;
+    } catch (error) {
+      console.log("requesting error", error);
+    }
+  }
+
   useEffect(() => {
     fetchPokemons();
-  },[page]);
+    fetchPokemonsByType();
+  }, [page]);
 
   const onSearchHandler = async (pokemon) => {
     if (!pokemon) {
       return fetchPokemons();
     }
-
     setIsLoading(true);
     setNotFound(false);
     const result = await searchPokemons(pokemon);
@@ -74,17 +91,39 @@ function App() {
     setIsLoading(false);
   };
 
+  const onSelectHandler = async (type) => {
+    setIsLoading(true);
+    setNotFound(false);
+    const result = await filterPokemonsByType(type);
+    if (!result) {
+      setNotFound(true);
+    } else {
+      const sanitizedResult = result.pokemon.map((element) => element.pokemon)
+      const promises = sanitizedResult.map(async (pokemon) => {
+        return await getPokemonData(pokemon.url);
+      });
+      const enrichedResult = await Promise.all(promises);
+      setPokemons(enrichedResult);
+    }
+    setIsLoading(false);
+  }
+
   return (
     <>
       <ThemeProvider theme={themes[currentTheme]}>
         <GlobalStyle />
         <Logo />
         <Toggle switchTheme={switchTheme} currentTheme={currentTheme} />
-        <SearchBar onSearchHandler={onSearchHandler} />
+        <Container>
+          <Text variant="subtitle">Select the pokémons by type: </Text>
+          <SelectByType onSelectHandler={onSelectHandler} pokemonsTypes={pokemonsTypes} />
+          <Text variant="subtitle">or Search your favorite pokémon by name or id: </Text>
+          <SearchBar onSearchHandler={onSearchHandler} search={search} setSearch={setSearch} />
+        </Container>
         {notFound ? (
           <Text variant="subtitle">Pokémon not found, try again </Text>
         ) : (
-          <Cards pokemons={pokemons} isLoading={isLoading} page={page} setPage={setPage} totalPages={totalPages}/>
+          <Cards pokemons={pokemons} isLoading={isLoading} page={page} setPage={setPage} totalPages={totalPages} />
         )}
       </ThemeProvider>
     </>
