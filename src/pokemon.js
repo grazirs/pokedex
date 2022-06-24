11ec0e5c52922f3d0734c029a97ba001a3fbaa52
Graziele Ribeiro <graziribeiro95@gmail.com>
2022-06-24 15:19:58 -0300

    create pokemon class

---
+++ src/pokemon.js
@@ -0,0 +1,13 @@
+export class Pokemon {
+  constructor(
+    id,
+    name,
+    types,
+    image,
+  ) {
+    this.id = id;
+    this.name = name;
+    this.types = types;
+    this.image = image;
+  }
+}
